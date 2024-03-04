7cc3d44f955a7ec53ef69a6a1bda23ddf5751bfd
AB <a.bierler@xdev-software.de>
2024-03-04 09:33:55 +0100

    Cleanup

--- src/main/java/software/xdev/openrewriter/executor/request/recipedata/simpleartifact/SimpleArtifactRecipesData.java
+++ src/main/java/software/xdev/openrewriter/executor/request/recipedata/simpleartifact/SimpleArtifactRecipesData.java
@@ -1,7 +1,6 @@
 package software.xdev.openrewriter.executor.request.recipedata.simpleartifact;
 
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 import software.xdev.openrewriter.executor.request.recipedata.RecipesData;
@@ -9,28 +8,18 @@ import software.xdev.openrewriter.executor.request.recipedata.RecipesData;
 
 public class SimpleArtifactRecipesData implements RecipesData
 {
-	private Set<Artifact> artifacts = new HashSet<>();
+	private final Set<Artifact> artifacts = new HashSet<>();
 	
-	private Set<Recipe> recipes = new HashSet<>();
+	private final Set<Recipe> recipes = new HashSet<>();
 	
 	public Set<Artifact> getArtifacts()
 	{
 		return this.artifacts;
 	}
 	
-	public void setArtifacts(final Set<Artifact> artifacts)
-	{
-		this.artifacts = Objects.requireNonNull(artifacts);
-	}
-	
 	public Set<Recipe> getRecipes()
 	{
 		return this.recipes;
-	}
-	
-	public void setRecipes(final Set<Recipe> recipes)
-	{
-		this.recipes = Objects.requireNonNull(recipes);
 	}
 	
 	@Override
